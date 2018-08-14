776c3aeb2416c012c7dd453d0531fd59df0e3c1f
Brian Nelson <nelsonsbrian@gmail.com>
2018-08-13 18:06:18 -0700

    add input field to replace vowels with dashes. Display results on output field div

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,21 @@
+function replaceVowel(phrase) {
+    alert(phrase);
+    return phrase.replace(/['a', 'e', 'i', 'o', 'u']/gi, '-');
+
+}
+
+
 $(document).ready(function() {
 
-  $("#countUpForm").submit(function(event) {
+  $("#txtInputForm").submit(function(event) {
     event.preventDefault();
 
+    var userInput = $("input#txtInput").val();
+    var userInputArray = userInput;
 
-    var countTo = Math.abs(parseInt($("#input1").val()));
-    var countBy = Math.abs(parseInt($("#input2").val()));
-
-    if (isNaN(countTo) || isNaN(countBy) || countTo === undefined || countBy === undefined) {
-      alert("Enter a number in both fields.")
-    } else if (countBy > countTo) {
-      alert("Count by number needs to be smaller than count to.")
-    } else {
-      $(".output").append("Count to: " + countTo + "<br>");
-      $(".output").append("Count by: " + countBy + "<br>");
-
-      var loops = Math.floor(countTo / countBy);
-      var runningCount = 0;
-      for (i = 0; i < loops; i++) {
-        runningCount += countBy;
-        $(".output").append(runningCount + ", ");
-      }
-
-
-
-    }
-
+    $(".output").text(replaceVowel(userInputArray));
+    $(".toHide").hide();
+    $('.output').show();
   });
 
 
